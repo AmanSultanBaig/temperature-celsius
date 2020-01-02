8769859f32205357a278b614a25c329c83f0754a
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-01-03 00:04:50 +0500

    temperature-celsius implemented

---
+++ celsius.js
@@ -0,0 +1,6 @@
+// get temperature from user
+let temperatureInput = prompt(); 
+// do celsius for current temperature
+let Celsius = (temperatureInput - 32) * 5 / 9;
+// get output 
+console.log(Celsius);
