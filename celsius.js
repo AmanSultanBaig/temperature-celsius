a0907fcbc9698e0010746cc72c5cb222504ca403
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-01-03 00:06:39 +0500

    Update celsius.js

--- celsius.js
+++ celsius.js
@@ -1,6 +1,6 @@
-// get temperature from user
+// get temperature fahrenheit from user
 let temperatureInput = prompt(); 
-// do celsius for current temperature
+// do celsius for current temperature fahrenheit
 let Celsius = (temperatureInput - 32) * 5 / 9;
 // get output 
 console.log(Celsius);
